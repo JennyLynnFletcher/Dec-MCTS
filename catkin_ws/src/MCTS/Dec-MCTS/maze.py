b67f0a7e10a6ec03b16cdd6ef6c433e96e329830
pondus314 <peter.ondus@gmail.com>
2022-04-23 23:43:15 +0100

    maze stuff second try

--- catkin_ws/src/MCTS/Dec-MCTS/maze.py
+++ catkin_ws/src/MCTS/Dec-MCTS/maze.py
@@ -6,91 +6,104 @@ from scipy import sparse
 import maze_gen
 
 
-class Maze():
-    def simulate_i_steps(self,steps, agent_id, policy):
+class Maze:
+    def __init__(self, goal, maze_walls):
+        self.goal = goal
+        self.agent_positions = {}
+        self.walls = maze_walls
+
+    def simulate_i_steps(self, steps, agent_id, policy):
         valid_policy = True  # If policy conflicts with facts, end at last valid position and default
         for i in range(steps):
             if i < len(policy) and valid_policy:
-                if self.valid_action(agent_id, policy[i]):
-                    self.execute_action(agent_id, policy[i])
-                else:
+                if not self.try_action(agent_id, policy[i]):
                     valid_policy = False
 
             # Inverse so that changing policy to invalid immediately jumps here
             if not (i < len(policy) and valid_policy):
                 self.execute_default_action(agent_id)
 
-    def valid_action(self, agent_id, action):
-        pass
-
-    def execute_action(self, agent_id, action):
-        pass
+    def try_action(self, agent_id, action):
+        x, y = self.agent_positions[agent_id]
+        new_position = [x, y]
+        if action == Action.UP:
+            new_position[1] -= 1
+        elif action == Action.DOWN:
+            new_position[1] += 1
+        elif action == Action.RIGHT:
+            new_position[0] += 1
+        elif action == Action.LEFT:
+            new_position[0] -= 1
+        if self.walls[new_position]:
+            self.agent_positions[agent_id] = new_position
+            return True
+        else:
+            return False
 
     def execute_default_action(self, agent_id):
-        pass
+        self.try_action(agent_id, Action.STAY)
 
-    # loc is (x,y) tuple
-    def add_robot(self, id, loc):
-        pass
+    def get_score(self, agent_obs, comms_aware=False):
+        w = self.walls.shape[1]
+        h = self.walls.shape[0]
+        obs_all = list(agent_obs.values())
+        positions_all = list(self.agent_positions.values())
 
-    def get_score(self, comms_aware=False):
-        # TODO: replace code starting here with proper code once stuff is decided
-        # ----------------- FILLER CODE START -----------------
-        enviornment = None
-        env = enviornment.Environment(7, 7, maze_gen.generate_maze(5, 5), (1, 1), (5, 5))
-        agent_obs = {5: sparse.dok_matrix((7, 7)), 1: sparse.dok_matrix((7, 7))}  # id to dok matrix obs should work
-        robot_locs = [(3, 3), (1, 1)]
-        # ------------------ FILLER CODE END ------------------
-        goal = env.goal
-        w = env.width
-        h = env.height
-        # TODO: figure out how to deal with height and width of maze matrix being + 2 sized, this may be dumb
-        explored = sparse.dok_matrix(sum(agent_obs.values(), start=sparse.dok_matrix((h, w))), dtype=bool)
+        explored = obs_all[0] == 1
+        for obs in obs_all:
+            explored = explored.maximum(obs == 1)
+
         total_explorable_area = ((h - 2) * (w - 2) + h + w - 5) / 2
         percent_explored = len(explored) / total_explorable_area
 
         distance = sparse.dok_matrix((h, w), dtype=int)
         visited = sparse.dok_matrix((h, w), dtype=bool)
-        visited[goal] = True
+        visited[self.goal] = True
         goal_connected = sparse.dok_matrix((h, w), dtype=bool)
-        goal_connected[goal] = True
+        goal_connected[self.goal] = True
         bfs_queue = collections.deque()
-        bfs_queue.append(goal)
+        bfs_queue.append(self.goal)
+        max_distance = 0
         while bfs_queue:
             current = bfs_queue.pop()
             y, x = current
             for dif in (-1, 1):
                 for neighbour in [(y + dif, x), (y, x + dif)]:
-                    if visited[neighbour] or (not env.walls[neighbour]):
+                    if visited[neighbour] or (not self.walls[neighbour]):
                         continue
                     visited[neighbour] = True
                     distance[neighbour] = distance[current] + 1
+                    max_distance = max(distance[neighbour], max_distance)
                     goal_connected[neighbour] = goal_connected[current] and (not explored[neighbour])
                     bfs_queue.append(neighbour)
 
-        goal_component_size = len(goal_connected)
-        robot_distances = [distance.T[robot_loc] for robot_loc in robot_locs]
+        goal_component_size_pct = len(goal_connected) / total_explorable_area
+        robot_distances = [distance.T[position] for position in self.agent_positions]
 
         # TODO: think about the weights on these things, for now they are all equal
         #  which is a really bad idea since percent explored < 1 and others are >> 1
-        score = sum(robot_distances) + percent_explored + goal_component_size
+        score = sum(robot_distances)/(len(robot_distances) * max_distance) + percent_explored + goal_component_size_pct
 
         if comms_aware:
             max_dist = 0
             sum_dists = 0
             n_dists = 0
-            for i in range(len(robot_locs)):
+            for i in range(len(positions_all)):
                 for j in range(i):
-                    x1, y1 = robot_locs[i]
-                    x2, y2 = robot_locs[j]
+                    x1, y1 = positions_all[i]
+                    x2, y2 = positions_all[j]
                     dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
 
                     max_dist = max(max_dist, dist)
                     n_dists += 1
                     sum_dists += dist
 
-            score += max_dist
+            score += max_dist / max_distance
         return score
+
+    # loc is (x,y) tuple
+    def add_robot(self, id, loc):
+        self.agent_positions[id] = loc
 
 
 class Action(Enum):
@@ -102,4 +115,5 @@ class Action(Enum):
 
 
 def generate_maze(obs, goal):
-    return None
+    maze_walls = maze_gen.fill_in_maze(obs)
+    return Maze(goal, maze_walls)
