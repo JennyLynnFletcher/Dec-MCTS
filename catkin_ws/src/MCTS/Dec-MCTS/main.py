d3ff7d5f4b7fbaa595f30699ba206e80dc6aaa16
pondus314 <peter.ondus@gmail.com>
2022-04-24 19:02:06 +0100

    mend a bug

--- catkin_ws/src/MCTS/Dec-MCTS/main.py
+++ catkin_ws/src/MCTS/Dec-MCTS/main.py
@@ -10,14 +10,13 @@ import environment
 if __name__ == '__main__':
     pub = rospy.Publisher('tick', Empty, queue_size=10)
     rospy.init_node('Main', anonymous=True)
-    
-    env = environment.Environment(15,15,(6,7), 10, render_interval=1)
-    
-    env.add_robot(0, (1,1), (5,5))
-    env.add_robot(1, (1,1), (5,5))
-    
-    
-    
+
+    goal = (5, 5)
+    env = environment.Environment(15, 15, goal, 10, render_interval=1)
+
+    env.add_robot(0, (1, 1), goal)
+    env.add_robot(1, (1, 1), goal)
+
     while True:
         pub.publish(Empty())
         time.sleep(0.2)
