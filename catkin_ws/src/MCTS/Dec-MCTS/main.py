f86271b892fbe39952e74a0993bc363e8f70eed2
JennyLynnFletcher <jenny.lynn.fletcher@gmail.com>
2022-04-26 18:46:34 +0100

    changed to makedirs

--- catkin_ws/src/MCTS/Dec-MCTS/main.py
+++ catkin_ws/src/MCTS/Dec-MCTS/main.py
@@ -19,12 +19,13 @@ def main(comms_aware=True, num_robots=3, seed=0, name="default"):
     rospy.init_node('Main', anonymous=True)
 
     if not os.path.exists("output/" + name):
-        os.mkdir("output/" + name)
+        os.makedirs("output/" + name)
 
-    width = 21
-    height = 21
+    width = 11
+    height = 11
 
     goal = (random.randrange(0, width // 2) * 2 + 1, random.randrange(0, height // 2) * 2 + 1)
+    print("Goal: ", goal)
     random.seed(seed)
     env = environment.Environment(width, height, goal, num_robots, render_interval=1, seed=0)
 
@@ -82,5 +83,5 @@ def main(comms_aware=True, num_robots=3, seed=0, name="default"):
 
 
 for i in range(10):
-    main(comms_aware=True, num_robots=10, seed=i, name="21x21_comms_" + str(i))
-    main(comms_aware=False, num_robots=10, seed=i, name="21x21_nocoms_" + str(i))
+    main(comms_aware=True, num_robots=3, seed=i, name="11x11_comms_" + str(i))
+    main(comms_aware=False, num_robots=3, seed=i, name="11x11_nocoms_" + str(i))
