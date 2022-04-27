d23e18561bad6b3f2f8a11112aa59b07977132bd
Iris Kutsyy <ak2149@cam.ac.uk>
2022-04-27 15:15:34 +0100

    bug hunting

--- catkin_ws/src/MCTS/Dec-MCTS/main.py
+++ catkin_ws/src/MCTS/Dec-MCTS/main.py
@@ -15,6 +15,7 @@ import pygame
 
 import environment
 import decMCTS
+import logging
 
 
 def call_update(x):
@@ -39,7 +40,6 @@ def main(comms_aware=True, num_robots=3, seed=0, name="default", out_of_date_tim
     print("Goal: ", goal)
     env = environment.Environment(width, height, goal, num_robots, render_interval=1, seed=seed, name=name)
 
-
     robot_start_locations = []
     for _ in range(num_robots):
         loc = (random.randrange(0, width // 2) * 2 + 1, random.randrange(0, height // 2) * 2 + 1)
@@ -50,7 +50,7 @@ def main(comms_aware=True, num_robots=3, seed=0, name="default", out_of_date_tim
     robots = []
     for robot_id, start_location in enumerate(robot_start_locations):
         env.add_robot(robot_id, start_location, goal)
-        #rospy.Subscriber('robot_obs_'+name, String, lambda x: robots[-1].reception_queue.append(x))
+        # rospy.Subscriber('robot_obs_'+name, String, lambda x: robots[-1].reception_queue.append(x))
 
     env.set_up_listener()
 
@@ -94,28 +94,39 @@ def main(comms_aware=True, num_robots=3, seed=0, name="default", out_of_date_tim
     pygame.quit()
     with open('./results.txt', 'a') as f:
         fcntl.flock(f, fcntl.LOCK_EX)
-        f.write("Seed: " + str(seed) + " comms_aware: " + str(comms_aware) + " num_robots: " + str(num_robots) + " Iterations: " + str(i) + " Times forgot other agent: "
-                + str([agent.times_removed_other_agent for agent in robots])+"\n")
+        f.write("Seed: " + str(seed) + " comms_aware: " + str(comms_aware) + " num_robots: " + str(
+            num_robots) + " Iterations: " + str(i) + " Times forgot other agent: "
+                + str([agent.times_removed_other_agent for agent in robots]) + "\n")
         fcntl.flock(f, fcntl.LOCK_UN)
 
+
 if __name__ == "__main__":
-    try:
-        if len(sys.argv) > 1:
+    if len(sys.argv) > 1:
+        try:
+
             print(sys.argv)
+
             name = sys.argv[1]
+
             seed = int(sys.argv[2])
             num_robots = int(sys.argv[3])
             maze_width = int(sys.argv[4])
             comms = sys.argv[5].strip() == "True"
             out_of_date_timeout = int(sys.argv[6]) if int(sys.argv[6]) > 0 else None
 
+            fullname = name + "__" + str(maze_width) + "x" + str(maze_width) + "__comms_" + str(
+                     comms) + "__timeout_" + str(
+                     out_of_date_timeout) + "__seed_" + str(seed)
+            logging.basicConfig(filename='./MyLog'+fullname+'.log', level=logging.DEBUG)
+
+
             main(comms_aware=comms, num_robots=num_robots, seed=seed,
-                 name=name + "__" + str(maze_width) + "x" + str(maze_width) + "__comms_" + str(comms) + "__timeout_" + str(
-                     out_of_date_timeout) + "__seed_" + str(seed),
+                 name=fullname,
                  out_of_date_timeout=out_of_date_timeout)
-        else:
-            for i in range(10):
-                main(comms_aware=True, num_robots=5, seed=i, name="11x11_comms" + str(i))
-                main(comms_aware=False, num_robots=5, seed=i, name="11x11_nocomms" + str(i))
-    except SystemExit:
-        pygame.quit()
+        except Exception as e:
+            print(e)
+
+    else:
+        for i in range(10):
+            main(comms_aware=True, num_robots=5, seed=i, name="11x11_comms" + str(i))
+            main(comms_aware=False, num_robots=5, seed=i, name="11x11_nocomms" + str(i))
